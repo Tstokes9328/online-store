1ce4475c0342892890c3b80599e8ddbadc36ceba
Tayte Stokes <taytestokes9328@gmail.com>
2018-08-06 14:24:29 -0600

    Setup HashRouter for routing

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+//Components
+import Home from './components/Home/Home';
+
+//Routing
+export default (
+    <Switch>
+        <Route exact path="/" component={Home}/>
+    </Switch>
+)
