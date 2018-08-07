6bd92794e113da9ecbfb017f8b3c1ada99c0c32a
Tayte Stokes <taytestokes9328@gmail.com>
2018-08-06 23:50:03 -0600

    connected database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 app.use(bodyParser.json());
+
+//Environmental Vairables
+let {
+    CONNECTION_STRING
+    } = process.env;
+
+//Connection To Database w/ Massive
+massive(CONNECTION_STRING).then(db => {
+    console.log('Database Connected')
+    app.set('db', db)
+});
 
 app.listen(3005, () => console.log('Connected on port 3005'));
