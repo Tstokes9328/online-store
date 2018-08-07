87272f73e7adc2a38690cd9504c294bfe506b430
Tayte Stokes <taytestokes9328@gmail.com>
2018-08-06 20:55:52 -0600

    Desktop home header styled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,11 @@ class Home extends Component {
         return (
             <div className="home-container">
                 <div className="home-header">
-
+                <div className="desktop-skate-sale">
+                    <h1>New Boards Are Here</h1>
+                    <p>Our brand new boards are back in stock! Get them while the gettin' is good before it's too late.</p>
+                    <button>shop collection</button>
+                </div>
                 </div>
                 <div className="mobile-skate-sale">
                     <h1>New Boards Are Here</h1>
