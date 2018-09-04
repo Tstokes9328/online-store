ccea174d25ec1cf1b90a801e5ed3a08754b641c9
Tayte Stokes <taytestokes9328@gmail.com>
2018-09-03 21:11:49 -0600

    unsubscribed the app component from the redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
 
 //Styling
 import './App.css';
@@ -16,38 +15,19 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-//Redux Method
-import {getUser} from './redux/users';
-
 class App extends Component {
-
-  componentDidMount(){
-    this.props.getUser();
-  }
-  
   render() {
     //adds font awesome icons into a library to use
     library.add(faShoppingCart, fab)
-
-    console.log(this.props)
 
     return (
       <div className="app-container">
         <Navbar />
         {Routes}
-        <a href={process.env.REACT_APP_LOGIN}>
-        <button>Click</button>
-        </a>
         <Footer />
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
-  return {
-    user: state.user
-  }
-}
-
-export default connect(mapStateToProps, {getUser})(App);
+export default App;
